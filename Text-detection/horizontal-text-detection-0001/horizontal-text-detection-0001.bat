251a14be7a19c21be9b16f2b578bd0b242f3e8ab
Kent.Yao <Kent.Yao@advantech.com>
2020-12-01 16:27:18 +0800

    add zip files

---
+++ Text-detection/horizontal-text-detection-0001/horizontal-text-detection-0001.bat
@@ -0,0 +1,3 @@
+text_detection_demo -i pic.png -loop -m_td horizontal-text-detection-0001.xml  
+
+ 
---
+++ Text-detection/text-detection1/text_detection_demo.bat
@@ -0,0 +1,3 @@
+text_detection_demo -i pic.png -loop -m_td text-detection-0003.xml -m_tr text-recognition-0012.xml  
+
+ 
